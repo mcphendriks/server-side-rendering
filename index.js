42a64e3e90e32a7f7df1041ae3cd03d2b7481b42
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-03-06 17:48:07 +0100

    docs: include files/folders

---
+++ index.js
@@ -0,0 +1,9 @@
+// Importeert express uit de node_modules map
+const express = require("express");
+console.log(express);
+
+// Haalt gegevens op van de REST API
+
+// start de server en geeft het poortnummer door waarnaar de server luisterd
+app.listen("port", process.env.port || 8000);
+console.log(listen);
