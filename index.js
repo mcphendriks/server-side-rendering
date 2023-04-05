4f6fe78dafb9454fc705adfde68339b5f1299667
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-04-05 15:01:45 +0200

    refactor code

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("./public"));
 
-// fetch data uit de api
-const url = "https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes";
-const data = await fetch(url).then((response) => response.json());
-
 //  Maak een route voor de index
 app.get("/", function (req, res) {
   res.render("index", data);
 });
 
+// fetch data uit de api
+const url = "https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes";
+const data = await fetch(url).then((response) => response.json());
+
 // Start express op, haal het ingestelde poortnummer op
+app.set("port", 700);
 app.listen(app.get("port"), function () {
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
